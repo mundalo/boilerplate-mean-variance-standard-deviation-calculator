fef63c8d4228c131774f0b3f969c5c2f5b472b99
Miriam Olesen <molesen@student.hive.fi>
2025-02-22 17:08:19 +0000

    fixed rounding errors

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,17 +1,41 @@
 import numpy as np
 
 def calculate(list):
-    arr = np.array(list)
-    if (arr.size != 9):
+    if (len(list) != 9):
         raise ValueError("List must contain nine numbers.")
-    reshaped = np.reshape(arr, (3, 3))
+    arr = np.array(list).reshape(3, 3)
+
     calculations = {
-        "mean": [np.mean(reshaped, axis=0), np.mean(reshaped, axis=1), np.mean(reshaped)],
-        'variance': [np.var(reshaped, axis=0), np.var(reshaped, axis=1), np.var(reshaped)],
-        'standard deviation': [np.std(reshaped, axis=0), np.std(reshaped, axis=1), np.std(reshaped)],
-        'max': [np.max(reshaped, axis=0), np.max(reshaped, axis=1), np.max(reshaped)],
-        'min': [np.min(reshaped, axis=0), np.min(reshaped, axis=1), np.min(reshaped)],
-        'sum': [np.sum(reshaped, axis=0), np.sum(reshaped, axis=1), np.sum(reshaped)]
+        "mean": [
+            np.mean(arr, axis=0).tolist(), 
+            np.mean(arr, axis=1).tolist(), 
+            np.mean(arr).tolist()
+        ],
+        'variance': [
+            np.var(arr, axis=0).tolist(), 
+            np.var(arr, axis=1).tolist(), 
+            np.var(arr).tolist()
+        ],
+        'standard deviation': [
+            np.std(arr, axis=0).tolist(), 
+            np.std(arr, axis=1).tolist(), 
+            np.std(arr).tolist()
+        ],
+        'max': [
+            np.max(arr, axis=0).tolist(), 
+            np.max(arr, axis=1).tolist(), 
+            np.max(arr).tolist()
+        ],
+        'min': [
+            np.min(arr, axis=0).tolist(), 
+            np.min(arr, axis=1).tolist(),
+            np.min(arr).tolist()
+        ],
+        'sum': [
+            np.sum(arr, axis=0).tolist(), 
+            np.sum(arr, axis=1).tolist(), 
+            np.sum(arr).tolist()
+        ]
     }
 
     return calculations
